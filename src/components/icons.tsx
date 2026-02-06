60d2df480ae56ece4096abf4434c1087bb0f4126
Kamandag Cudas <kamandagcudas@gmail.com>
2026-02-06 00:11:17 +0000

    https://www.google.com/imgres?q=black%20flame&imgurl=https%3A%2F%2Fimage

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,3 +1,4 @@
+
 import type { SVGProps } from 'react';
 
 /* This is the creator easter egg badge */
@@ -62,22 +63,13 @@ export function AngelicPowerRuneIcon(props: SVGProps<SVGSVGElement>) {
 }
 
 /* This is the Raytheon easter egg insignia */
-export function BlackFlameIcon(props: SVGProps<SVGSVGElement>) {
+export function BlackFlameIcon(props: any) {
     return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="1em"
-            height="1em"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="1.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+        <img
+            src="https://images.vexels.com/media/users/3/137273/isolated/lists/48d116c43afc367a1ee9ec7917a11b44-flame-fire-black-silhouette.png"
+            alt="Black Flame Insignia"
             {...props}
-        >
-            <path d="M12 2c2.4 4.5 7 8.3 7 12.5c0 3.1-2.2 5.5-5 5.5s-5-2.4-5-5.5c0-4.2 4.6-8 7-12.5z" stroke="hsl(var(--foreground))" fill="hsl(var(--foreground))" className="animate-black-flame-main" />
-            <path d="M10.1 13.9c-1-1.3-1.6-2.9-1.6-4.4c0-2.8 1.8-5.2 4.5-5.2s4.5 2.4 4.5 5.2c0 1.5-.6 3.1-1.6 4.4" stroke="hsl(var(--primary))" fill="hsl(var(--primary))" className="animate-black-flame-inner" />
-        </svg>
+            className={`animate-black-flame-main ${props.className || ''}`}
+        />
     );
 }
