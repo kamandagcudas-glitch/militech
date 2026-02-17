3d0b75bfe989477f9d237d9f860518bca46aeac9
Kamandag Cudas <kamandagcudas@gmail.com>
2026-02-17 16:52:26 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3572083579-72c91",
+  "appId": "1:847594821206:web:20ca893b3c7759b1ff4df7",
+  "apiKey": "AIzaSyDR6-NzK-TN0zpv04h-YFgAJmUJFyVSTT8",
+  "authDomain": "studio-3572083579-72c91.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "847594821206"
+};
