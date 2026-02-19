f340bdcf757aa90bda389b731708a52a1ad34235
Kamandag Cudas <kamandagcudas@gmail.com>
2026-02-19 14:26:48 +0000

    tell me my firebase api that i can put on netlify, currently at Environm

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3572083579-72c91",
-  "appId": "1:847594821206:web:20ca893b3c7759b1ff4df7",
-  "apiKey": "AIzaSyDR6-NzK-TN0zpv04h-YFgAJmUJFyVSTT8",
-  "authDomain": "studio-3572083579-72c91.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "847594821206"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDR6-NzK-TN0zpv04h-YFgAJmUJFyVSTT8",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-3572083579-72c91.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-3572083579-72c91",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "847594821206",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:847594821206:web:20ca893b3c7759b1ff4df7",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 };
